1e8e13e308fecaad499d2651cb939df65a28c43c
Steven Loria <sloria1@gmail.com>
2014-03-14 17:43:50 -0400

    Add model and routes boilerplate

--- {{cookiecutter.short_name | lower}}/__init__.py
+++ {{cookiecutter.short_name | lower}}/__init__.py
@@ -2,8 +2,11 @@ from . import model
 from . import routes
 from . import views
 
-MODELS = [model.Addon{{cookiecutter.short_name | capitalize}}NodeSettings]
-#USER_SETTINGS_MODEL = model.AddonUserSettings
+MODELS = [
+    model.Addon{{cookiecutter.short_name | capitalize}}UserSettings,
+    model.Addon{{ cookiecutter.short_name | capitalize }}NodeSettings,
+]
+USER_SETTINGS_MODEL = model.Addon{{ cookiecutter.short_name | capitalize }}UserSettings
 NODE_SETTINGS_MODEL = model.Addon{{ cookiecutter.short_name | capitalize }}NodeSettings
 
 ROUTES = [routes.settings_routes]
@@ -11,7 +14,7 @@ ROUTES = [routes.settings_routes]
 SHORT_NAME = '{{cookiecutter.short_name | lower}}'
 FULL_NAME = '{{cookiecutter.full_name}}'
 
-OWNERS = ['node']  # user
+OWNERS = ['node']  # can include any of ['user', 'node']
 
 ADDED_TO = {
     'user': False,
@@ -19,21 +22,20 @@ ADDED_TO = {
 }
 
 VIEWS = []  # page, widget
-CONFIGS = ['node']  # User
+CONFIGS = ['node']  # any of ['user', 'node']
 
 CATEGORIES = [{{cookiecutter.categories}}]
 
 INCLUDE_JS = {
-    'widget': [],
     'page': [],
     'files': []
 }
 
 INCLUDE_CSS = {
-    'widget': [],
     'page': [],
+    'files': []
 }
 
-HAS_HGRID_FILES = False
+HAS_HGRID_FILES = True  # set to True for storage addons that display in HGrid
 #GET_HGRID_DATA = views.hgrid.{{cookiecutter.short_name}}_hgrid_data
 MAX_FILE_SIZE = 10  # MB
--- {{cookiecutter.short_name | lower}}/model.py
+++ {{cookiecutter.short_name | lower}}/model.py
@@ -1,1 +1,17 @@
-# TODO
+# -*- coding: utf-8 -*-
+"""Persistence layer for the {{ cookiecutter.short_name }} addon.
+"""
+
+from website.addons.base import AddonUserSettingsBase, AddonNodeSettingsBase
+
+
+class Addon{{ cookiecutter.short_name | capitalize }}UserSettings(AddonUserSettingsBase):
+    pass
+    # TODO
+
+
+class Addon{{ cookiecutter.short_name | capitalize }}NodeSettings(AddonNodeSettingsBase):
+    pass
+    # TODO
+
+
